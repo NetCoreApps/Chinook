65291b92e13f18b51a18c85ddf33c43db28b8483
Demis Bellot <demis.bellot@gmail.com>
2024-07-07 19:02:34 +0930

    Merge AutoQuery with Db as DbFactory needed in GenerateCrudServices

--- Chinook/Configure.Db.cs
+++ Chinook/Configure.Db.cs
@@ -3,27 +3,41 @@ using ServiceStack.OrmLite;
 
 [assembly: HostingStartup(typeof(Chinook.ConfigureDb))]
 
-namespace Chinook
-{
-    // Example Data Model
-    // public class MyTable
-    // {
-    //     [AutoIncrement]
-    //     public int Id { get; set; }
-    //     public string Name { get; set; }
-    // }
+namespace Chinook;
 
-    public class ConfigureDb : IHostingStartup
+// Example Data Model
+// public class MyTable
+// {
+//     [AutoIncrement]
+//     public int Id { get; set; }
+//     public string Name { get; set; }
+// }
+
+public class ConfigureDb : IHostingStartup
+{
+    public void Configure(IWebHostBuilder builder)
     {
-        public void Configure(IWebHostBuilder builder)
+        builder.ConfigureServices((context,services) =>
         {
-            builder.ConfigureServices((context,services) =>
-            {
-                services.AddSingleton<IDbConnectionFactory>(new OrmLiteConnectionFactory(
-                    context.Configuration.GetConnectionString("DefaultConnection")
-                    ?? "App_Data/chinook.sqlite",
-                    SqliteDialect.Provider));
+            var dbFactory = new OrmLiteConnectionFactory(
+                context.Configuration.GetConnectionString("DefaultConnection")
+                ?? "App_Data/chinook.sqlite",
+                SqliteDialect.Provider);
+                
+            services.AddSingleton<IDbConnectionFactory>(dbFactory);
+                
+            services.AddPlugin(new AutoQueryFeature {
+                MaxLimit = 1000,
+                IncludeTotal = true,
+                // Enable AutoGen to generate APIs and Models for RDBMS:
+                // GenerateCrudServices = new GenerateCrudServices {
+                //     DbFactory = dbFactory, 
+                //     AutoRegister = true,
+                //     AddDataContractAttributes = false,
+                // }
+                // Command to export all AutoGen Types into code-first dtos.cs
+                // $ x csharp https://localhost:5001 -path /crud/all/csharp
             });
-        }
+        });
     }
 }
