e7918965aea9570344e245e3981d2a3213578418
Demis Bellot <demis.bellot@gmail.com>
2024-07-07 19:02:45 +0930

    use file scoped namespaces

--- Chinook/Configure.AppHost.cs
+++ Chinook/Configure.AppHost.cs
@@ -4,33 +4,30 @@ using ServiceStack;
 
 [assembly: HostingStartup(typeof(Chinook.AppHost))]
 
-namespace Chinook
+namespace Chinook;
+
+public class AppHost() : AppHostBase("Chinook", typeof(MyServices).Assembly), IHostingStartup
 {
-    public class AppHost : AppHostBase, IHostingStartup
+    public void Configure(IWebHostBuilder builder) =>
+        builder.ConfigureServices(services => {
+            // Configure ASP.NET Core IOC Dependencies
+            services.AddPlugin(new CorsFeature(new[] {
+                    "http://localhost:3000",
+                    "https://docs.servicestack.net"
+                },
+                allowCredentials: true,
+                allowedHeaders: "Content-Type, Allow, Authorization"
+            ));
+        });
+
+    public override void Configure(Container container)
     {
-        public AppHost() : base("Chinook", typeof(MyServices).Assembly) { }
-
-        public void Configure(IWebHostBuilder builder) =>
-            builder.ConfigureServices(services => {
-                // Configure ASP.NET Core IOC Dependencies
-                services.AddPlugin(new CorsFeature(new[] {
-                        "http://localhost:3000",
-                        "https://docs.servicestack.net"
-                    },
-                    allowCredentials: true,
-                    allowedHeaders: "Content-Type, Allow, Authorization"
-                ));
-            });
-
-        public override void Configure(Container container)
-        {
-            SetConfig(new HostConfig {
-                // DebugMode = false,
-                UseSameSiteCookies = true
-            });
+        SetConfig(new HostConfig {
+            // DebugMode = false,
+            UseSameSiteCookies = true
+        });
             
-            ConfigurePlugin<UiFeature>(feature => 
-                feature.Info.BrandIcon = new ImageInfo { Uri = "/logo.svg", Cls = "w-8 h-8 mr-2" });
-        }
+        ConfigurePlugin<UiFeature>(feature => 
+            feature.Info.BrandIcon = new ImageInfo { Uri = "/logo.svg", Cls = "w-8 h-8 mr-2" });
     }
 }
